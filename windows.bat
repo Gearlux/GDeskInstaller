890085dbb28d2cb46f5f35c93a4de230a759e6b6
Gearlux <gert.behiels@gmail.com>
2015-02-10 21:46:03 +0100

    MacOsX installer

--- windows.bat
+++ windows.bat
@@ -75,7 +75,7 @@ REM rmdir /Q /S windows
 
 %QTIFW%\bin\repogen.exe -p installer\packages --update -e io.qt,com.microsoft.vcredist_2008,com.microsoft.vcredist_2013,com.slproweb.openssl windows
 REM %QTIFW%\bin\binarycreator.exe -c installer\config\config.xml -p installer\packages GDeskTunesInstaller.exe
-%QTIFW%\bin\binarycreator.exe -c installer\config\config.xml -p installer\packages -e io.qt,com.microsoft.vcredist_2008,com.microsoft.vcredist_2013,com.slproweb.openssl GDeskTunesSetup.exe
+%QTIFW%\bin\binarycreator.exe -c installer\config\windows_config.xml -p installer\packages -e io.qt,com.microsoft.vcredist_2008,com.microsoft.vcredist_2013,com.slproweb.openssl GDeskTunesSetup.exe
 
 move GDeskTunesInstaller.exe windows
 move GDeskTunesSetup.exe windows
