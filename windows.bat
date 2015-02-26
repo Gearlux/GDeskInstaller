9c7e46c9d239ff3b340f0d2d0b58cc05c0ac9768
Gearlux <gert.behiels@gmail.com>
2015-02-26 21:23:38 +0100

    Windows release

--- windows.bat
+++ windows.bat
@@ -31,43 +31,40 @@ for %%x in (%*) do (
 REM echo Number of processed arguments: %argCount%
 REM for /L %%i in (1,1,%argCount%) do echo %%i- "!argVec[%%i]!"
 
-if DEFINED CLEAN (
-	echo "Cleaning"
-	rmdir /S /Q build
-	mkdir build
-)
-
 REM Build GDeskTunes Completely
 cd build
 qmake ..\..\GDeskTunes\GDeskTunes.pro
+if DEFINED CLEAN (
+	nmake clean
+)
 nmake release install
 cd ..
 
 REM Create Qt distribution Files
-rmdir /S /Q GDeskTunes
-mkdir GDeskTunes
+rmdir /S /Q GDeskTunesTmp
+mkdir GDeskTunesTmp
 
-xcopy /S build\src\release\GDeskTunes.exe GDeskTunes
-windeployQt GDeskTunes\GDeskTunes.exe
-del /S GDeskTunes\GDeskTunes.exe
+xcopy /S build\src\release\GDeskTunes.exe GDeskTunesTmp
+windeployQt GDeskTunesTmp\GDeskTunes.exe
+del /S GDeskTunesTmp\GDeskTunes.exe
 
 rmdir /Q /S installer\packages\io.qt\data
 mkdir installer\packages\io.qt\data
 xcopy /S /Y GDeskTunes\* installer\packages\io.qt\data
 
 REM Create GDeskTunes program
-rmdir /S /Q GDeskTunes
-mkdir GDeskTunes
+rmdir /S /Q GDeskTunesTmp
+mkdir GDeskTunesTmp
 
-xcopy /S build\src\release\*.exe GDeskTunes
-xcopy /S build\src\release\*.dll GDeskTunes
-xcopy /S build\lib\mmshellhook\release\*.dll GDeskTunes
-xcopy /S build\src\release\js GDeskTunes\js\
-xcopy /S build\src\release\userstyles GDeskTunes\userstyles\
+xcopy /S build\src\release\*.exe GDeskTunesTmp
+xcopy /S build\src\release\*.dll GDeskTunesTmp
+xcopy /S build\lib\mmshellhook\release\*.dll GDeskTunesTmp
+xcopy /S build\src\release\js GDeskTunesTmp\js\
+xcopy /S build\src\release\userstyles GDeskTunesTmp\userstyles\
 
 rmdir /Q /S installer\packages\org.gearlux.gdesktunes\data
 mkdir installer\packages\org.gearlux.gdesktunes\data
-xcopy /S /Y GDeskTunes\* installer\packages\org.gearlux.gdesktunes\data
+xcopy /S /Y GDeskTunesTmp\* installer\packages\org.gearlux.gdesktunes\data
 
 REM del /Q GDeskTunesInstaller.exe
 REM del /Q GDeskSetup.exe
@@ -77,5 +74,5 @@ REM rmdir /Q /S windows
 %QTIFW%\bin\binarycreator.exe --offline-only -c installer\config\windows_config.xml -p installer\packages GDeskInstaller.exe
 %QTIFW%\bin\binarycreator.exe -c installer\config\windows_config.xml -p installer\packages -e io.qt,com.microsoft.vcredist_2008,com.microsoft.vcredist_2013,com.slproweb.openssl GDeskSetup.exe
 
-sREM move GDeskTunesInstaller.exe windows
+REM move GDeskTunesInstaller.exe windows
 move GDeskSetup.exe windows
